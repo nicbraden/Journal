565323cbbce36f7d656bd156b44a3c20cd05a881
nicbraden <50718937+nicbraden@users.noreply.github.com>
2020-10-06 17:47:14 -0400

    Update noteReducer.js

--- client/src/reducers/noteReducer.js
+++ client/src/reducers/noteReducer.js
@@ -1,44 +1,33 @@
-import { v1 as uuid } from "uuid";
-import { GET_NOTES, ADD_NOTE, DELETE_NOTE } from '../actions/types';
+import { GET_NOTES, ADD_NOTE, DELETE_NOTE, NOTES_LOADING } from '../actions/types';
 
 const initalState = {
-    notes: [
-        {
-            id: uuid(),
-            "title": "hello",
-            "mood": "hi",
-            "note": "welcome"
-        },
-        {
-            id: uuid(),
-            "title": "bye",
-            "mood": "cya",
-            "note": "goodbye"
-        },
-        {
-            id: uuid(),
-            "title": "hmm",
-            "mood": "huh",
-            "note": "Idunno"
-        }
-    ]
+    notes: [],
+    loading: false
 }
 
 export default function (state = initalState, action) {
     switch (action.type) {
         case GET_NOTES:
             return {
-                ...state
+                ...state,
+                //get notes from payload
+                notes: action.payload,
+                loading: false
             }
         case DELETE_NOTE:
             return {
                 ...state,
-                notes: state.notes.filter(note => note.id !== action.payload)
+                notes: state.notes.filter(note => note._id !== action.payload)
             }
         case ADD_NOTE:
             return {
                 ...state,
                 notes: [action.payload, ...state.notes]
+            }
+        case NOTES_LOADING:
+            return {
+                ...state,
+                loading: true
             }
         default:
             return state;
