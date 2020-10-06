b697ffb44d35eee4f01e706a145c62278935df2d
nicbraden <50718937+nicbraden@users.noreply.github.com>
2020-10-06 17:42:50 -0400

    Update noteActions.js

--- client/src/actions/noteActions.js
+++ client/src/actions/noteActions.js
@@ -1,24 +1,47 @@
-import { GET_NOTES, ADD_NOTE, DELETE_NOTE } from './types';
+import { GET_NOTES, ADD_NOTE, DELETE_NOTE, NOTES_LOADING } from './types';
+import axios from 'axios';
 
-export const getNotes = () => {
-    return {
-        //goes to note reducer to check type
-        type: GET_NOTES
-    };
+export const getNotes = () => dispatch => {
+    dispatch(setNotesLoading());
+    //goes to router
+    axios
+        .get('http://localhost:8080/api/notes')
+        .then(res =>
+            dispatch({
+                type: GET_NOTES,
+                payload: res.data
+            })
+        )
 };
 
-export const deleteNote = (id) => {
-    return {
-        //goes to note reducer to check type
-        type: DELETE_NOTE,
-        payload: id
-    };
+
+export const addNote = note => dispatch => {
+    axios
+        //note comes from modal which carrys the data
+        .post('http://localhost:8080/api/notes', note)
+        .then(res =>
+            dispatch({
+                type: ADD_NOTE,
+                //data is the new note from notes js sent to the reducer
+                payload: res.data
+            })
+        );
 };
 
-export const addNote = note => {
-    return {
-        //goes to note reducer to check type
-        type: ADD_NOTE,
-        payload: note
-    };
+export const deleteNote = id => dispatch => {
+    axios
+        .delete(`http://localhost:8080/api/notes/${id}`)
+        .then(res =>
+            dispatch({
+                type: DELETE_NOTE,
+                payload: id
+            })
+        );
 };
+
+
+export const setNotesLoading = () => {
+    return {
+        type: NOTES_LOADING
+    }
+}
